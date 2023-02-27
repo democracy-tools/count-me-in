4b7cedbf7921095977a3eb1d1f2cb8adb57a3972
effoeffi <effoeffi@gmail.com>
2023-02-27 11:38:24 +0200

    test

--- internal/announcement_test.go
+++ internal/announcement_test.go
@@ -17,7 +17,7 @@ func TestAnnouncement(t *testing.T) {
 
 	var buf bytes.Buffer
 	require.NoError(t, json.NewEncoder(&buf).Encode(map[string][]*internal.Announcement{
-		"announcement": {{
+		"announcements": {{
 			UserId:     "test",
 			UserDevice: internal.Device{Id: "test-1", Type: "iphone 14"},
 			SeenDevice: internal.Device{Id: "test-2", Type: "iphone 15"},
