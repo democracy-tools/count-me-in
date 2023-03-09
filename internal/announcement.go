2225eaa509e8e0df2ec90b77d6d9fbc1d7389e60
Gil Shapira <gshapira@gmail.com>
2023-03-09 20:43:19 +0200

    Update announcement time validation

--- internal/announcement.go
+++ internal/announcement.go
@@ -84,7 +84,7 @@ func validateAnnouncements(announcements []*Announcement) bool {
 
 func validateAnnouncement(announcement *Announcement) bool {
 
-	if announcement.Time < 1678133631 || announcement.Time > time.Now().Add(time.Hour).Unix() {
+	if announcement.Time < 1678133631 || announcement.Time > time.Now().Add(time.Hour*2).Unix() {
 		log.Infof("invalid announcement time '%d' user '%s' user device '%s'",
 			announcement.Time, announcement.UserId, announcement.UserDevice.Id)
 		return false
